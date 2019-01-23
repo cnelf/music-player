9afc889ca2621db6ee004ab7898934d626b9f91a
CNelfHope <1184974829@qq.com>
2019-01-24 00:34:30 +0800

    init page add header and tab-router

---
+++ src/components/rank/rank.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="wrapper">
+    排行页面
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+</style>
