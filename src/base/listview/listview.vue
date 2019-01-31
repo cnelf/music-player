0e19e863c18eee3e3727afa196196b8cdd421788
CNelfHope <1184974829@qq.com>
2019-01-31 12:19:31 +0800

    add singer listview

---
+++ src/base/listview/listview.vue
@@ -0,0 +1,63 @@
+<template>
+  <scroll :data="data" class="listview">
+    <ul>
+      <li class="list-group" v-for="(group, index) in data" :key="index">
+        <h2 class="list-group-title">{{group.title}}</h2>
+        <ul>
+          <li class="list-group-item" v-for="(item, index) in group.items" :key="index">
+            <img class="avatar" v-lazy="item.avatar" alt="item.name">
+            <span class="name">{{item.name}}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </scroll>
+</template>
+
+<script type="text/ecmascript-6">
+import Scroll from 'base/scroll/scroll'
+
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    }
+  },
+  components: {
+    Scroll
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import "~common/stylus/variable"
+
+  .listview
+    position: relative
+    width: 100%
+    height: 100%
+    overflow: hidden
+    background: $color-background
+    .list-group
+      padding-bottom: 30px
+      .list-group-title
+        height: 30px
+        line-height: 30px
+        padding-left: 20px
+        font-size: $font-size-small
+        color: $color-text-l
+        background: $color-highlight-background
+      .list-group-item
+        display: flex
+        align-items: center
+        padding: 20px 0 0 30px
+        .avatar
+          width: 50px
+          height: 50px
+          border-radius: 50%
+        .name
+          margin-left: 20px
+          color: $color-text-l
+          font-size: $font-size-medium
+</style>
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="singer">
-    歌手页面
+    <listview :data="singers"></listview>
   </div>
 </template>
 
@@ -8,6 +8,7 @@
 import {getSingerList} from 'api/singer'
 import {ERR_OK} from 'api/config'
 import Singer from 'common/js/singer'
+import Listview from 'base/listview/listview'
 
 const HOT_NAME = '热门'
 const HOT_SINGER_LEN = 10
@@ -25,8 +26,7 @@ export default {
     _getSingerList() {
       getSingerList().then((res) => {
         if (res.code === ERR_OK) {
-          this.singers = res.data.list
-          console.log(this._normalizeSingerList(this.singers))
+          this.singers = this._normalizeSingerList(res.data.list)
         }
       })
     },
@@ -72,6 +72,9 @@ export default {
       })
       return hot.concat(ret)
     }
+  },
+  components: {
+    Listview
   }
 }
 </script>
