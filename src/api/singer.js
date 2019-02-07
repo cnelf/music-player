ab8927c0ee6b5d2ad8b28c2d1ad4dc16feff5c7d
CNelfHope <1184974829@qq.com>
2019-02-07 16:20:27 +0800

    add singer detail get song data

--- src/api/singer.js
+++ src/api/singer.js
@@ -17,3 +17,39 @@ export function getSingerList() {
 
   return jsonp(url, data, options)
 }
+
+export function getSingerDetail(singerId) {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    hostUin: 0,
+    needNewCode: 0,
+    platform: 'yqq',
+    order: 'listen',
+    begin: 0,
+    num: 100,
+    songstatus: 1,
+    g_tk: 1664029744,
+    singermid: singerId
+  })
+
+  return jsonp(url, data, options)
+}
+
+export function getSongVkey(songmid) {
+  const url = 'https://c.y.qq.com/base/fcgi-bin/fcg_music_express_mobile3.fcg'
+  const data = Object.assign({}, {
+    callback: 'musicJsonCallback',
+    loginUin: 3051522991,
+    format: 'jsonp',
+    platform: 'yqq',
+    needNewCode: 0,
+    cid: 205361747,
+    uin: 3051522991,
+    guid: 5931742855,
+    songmid: songmid,
+    filename: `C400${songmid}.m4a`
+  })
+
+  return jsonp(url, data)
+}
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -6,15 +6,50 @@
 
 <script type="text/ecmascript-6">
 import {mapGetters} from 'vuex'
+import {getSingerDetail, getSongVkey} from 'api/singer'
+import {ERR_OK} from 'api/config'
+import {createSong} from 'common/js/song'
 
 export default {
+  data() {
+    return {
+      songs: []
+    }
+  },
   created() {
-    console.log(this.singer)
+    this._getDetail()
   },
   computed: {
     ...mapGetters([
       'singer'
     ])
+  },
+  methods: {
+    _getDetail() {
+      if (!this.singer.id) {
+        this.$router.push('/singer')
+        return
+      }
+      getSingerDetail(this.singer.id).then((res) => {
+        if (res.code === ERR_OK) {
+          this.songs = this._normalizeSingerDetail(res.data.list)
+          console.log(this.songs)
+        }
+      })
+    },
+    _normalizeSingerDetail(list) {
+      let ret = []
+      list.forEach((item) => {
+        let {musicData} = item
+        getSongVkey(musicData.songmid).then((res) => {
+          const vkey = res.data.items[0].vkey
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData, vkey))
+          }
+        })
+      })
+      return ret
+    }
   }
 }
 </script>
