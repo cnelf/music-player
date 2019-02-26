377eb23de0f37ef776a6455f80cf17a7b5e412c5
CNelfHope <1184974829@qq.com>
2019-02-27 00:09:14 +0800

    add music search box

---
+++ src/base/search-box/search-box.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="search-box">
+    <i class="icon-search"></i>
+    <input class="box" type="text" :placeholder="placeholder" v-model="query">
+    <i class="icon-dismiss" v-show="query" @click="clear"></i>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  created() {
+    this.$watch('query', (newQuery) => {
+      this.$emit('query', newQuery)
+    })
+  },
+  data() {
+    return {
+      query: ''
+    }
+  },
+  props: {
+    placeholder: {
+      type: String,
+      default: '搜索歌曲、歌手'
+    }
+  },
+  methods: {
+    clear() {
+      this.query = ''
+    }
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import "~common/stylus/variable"
+
+  .search-box
+    display: flex
+    align-items: center
+    box-sizing: border-box
+    width: 100%
+    padding: 0 6px
+    height: 40px
+    background: $color-highlight-background
+    border-radius: 6px
+    .icon-search
+      font-size: 24px
+      color: $color-background
+    .box
+      flex: 1
+      margin: 0 5px
+      line-height: 18px
+      background: $color-highlight-background
+      color: $color-text
+      font-size: $font-size-medium
+      &::placeholder
+        color: $color-text-d
+    .icon-dismiss
+      font-size: 16px
+      color: $color-background
+</style>
